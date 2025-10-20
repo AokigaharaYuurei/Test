d9605c4799a1edad2c8420de1e3d6cc7557d1328
Unity <Unity@rbiu.local>
2025-10-20 13:02:21 +0500

    new 5

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('statuses')->insert(
+            [
+                [
+                    'name' => "новое"
+                ],
+                [
+                    'name' => "подтверждено"
+                ],
+                [
+                    'name' => "отклонено"
+                ]
+            ]
+            );
+    }
+}
