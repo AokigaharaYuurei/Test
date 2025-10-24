8d77ab4c4ae09c3f511eef866acf6959760152b9
Unity <Unity@rbiu.local>
2025-10-24 09:27:33 +0500

    fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Report;
+use App\Models\Status;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
-class ReportController extends Controller
-{
-    public function index(){
-    $reports = Report::paginate(8);
-    return view('report.index', compact('reports'));
+class ReportController extends Controller {
+
+public function index(Request $request){
+$sort = $request->input('sort');
+        if($sort !='asc' && $sort != 'desc'){
+            $sort = 'desc';
+        }
+
+        $status = $request->input('status');
+        $validate = $request->validate([
+            'status' => "exists:statuses,id"
+        ]);
+        if($validate){
+            $reports = Report::where('status_id', $status)
+            ->orderBy('created_at', $sort)
+            ->paginate(8);
+        } else{
+            $reports = Report::orderBy('created_at', $sort)
+            ->paginate(8);
+        }
+
+        $statuses = Status::all();
+
+        return view('reports.index', compact('reports', 'statuses', 'sort', 'status'));
+
+    
 }
 
 public function destroy(Report $report){
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -14,6 +14,23 @@
         </nav>
     </header>
     <main>
+        <div>
+                <span>Сортировка по дате создания: </span>
+                <a href="{{ route('reports.index', ['sort' => 'desc', 'status' => $status]) }}">сначала новые</a>
+                <a href="{{ route('reports.index', ['sort' => 'asc', 'status' => $status]) }}">сначала старые</a>
+            </div>
+            <div>
+                <p>Фильтрация по статусу заявки</p>
+                <ul>
+                    @foreach ($statuses as $status)
+                    <li>
+                        <a href="{{route('reports.index', ['sort' => $sort, 'status' => $status->id])}}">
+                            {{$status->name}}
+                        </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
         <div class="card">
             <p class="data">19.10.2024</p>
             <div class="text-line">
@@ -33,6 +50,7 @@
                 @endforeach
                 {{$reports->links()}}
             </div>
+            
         </div>
     </main>
 </body>
